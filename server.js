2f2e5bf900219811ae00a7276a4cb997f5e742f3
Kumar Manikandan <96768975+manikandanktcz@users.noreply.github.com>
2022-07-13 10:19:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,8 +53,7 @@ app.use(function (req, res, next) {
 // error handler
 // define as the last app.use callback
 
-// error handler
-// define as the last app.use callback
+
 app.use(function (err, req, res) {
   res.status(err.status || 500);
   res.send(err.message);
